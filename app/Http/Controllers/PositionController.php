b0b3dcfe6a499dc2c371f64a6a03ff7dc2072aaa
AndreyBelozertsev <andrey.belozertsev@gmail.com>
2025-05-01 13:21:04 +0300

    FirstCommit

--- app/Http/Controllers/PositionController.php
+++ app/Http/Controllers/PositionController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class PositionController extends Controller
 {
     public function test(Request $request){
+        Log::channel('requests')->info('Request logged:', [
+            'method' => $request->method(),
+            'url' => $request->fullUrl(),
+            'ip' => $request->ip(),
+            'user_agent' => $request->userAgent(),
+            'request_data' => $request->all(),
+        ]);
         dump($request);
     }
 }
